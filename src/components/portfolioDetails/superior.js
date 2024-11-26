90fc639d4cda655d979ee7b047cc8423fdf63dce
jatin478 <jatin@closedigit.com>
2024-11-26 18:36:34 +0530

    sticky tabbar and filtered tab content

--- src/components/portfolioDetails/superior.js
+++ src/components/portfolioDetails/superior.js
@@ -50,12 +50,7 @@ export default function Superior() {
                 />
             </div>
             <p className={portfolioDetailsCommon.bannerSecondaryText}>
-              At Superior Engineering, we specialize in creating custom number
-              plates and accessories tailored to your style. Our team combines
-              innovative design with high-quality materials to deliver unique
-              products that enhance your vehicle's personality. From concept to
-              creation, we ensure your satisfaction every step of the way.
-              Elevate your ride with our bespoke designs.
+            
             </p>
           </div>
         </div>
